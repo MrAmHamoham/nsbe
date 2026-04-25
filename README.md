98e460428ed654abf76c5dcebca32d709ded21eb
awruff <sillysys@proton.me>
2026-04-25 11:52:54 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Not Shit Block Entities
+
+Makes Block Entities use Baked Models rather than Entity Models.
+
+---
+
+[OSL-3.0](LICENSE)
---
+++ src/main/java/org/codeberg/awruff/nsbe/NotShitBlockEntities.java
@@ -0,0 +1,16 @@
+package org.codeberg.awruff.nsbe;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class NotShitBlockEntities implements ClientModInitializer {
+
+	public static final Logger LOGGER = LogManager.getLogger("Not Shit Block Entities");
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+
+}
