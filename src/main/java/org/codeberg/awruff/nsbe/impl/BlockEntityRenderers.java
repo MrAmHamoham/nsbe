8b40aa7c741ff27a0a464455335ae59cdc6b9e30
awruff <sillysys@proton.me>
2026-08-24 03:24:19 -0400

    compat: argentum

---
+++ src/main/java/org/codeberg/awruff/nsbe/impl/BlockEntityRenderers.java
@@ -0,0 +1,37 @@
+package org.codeberg.awruff.nsbe.impl;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.EnderChestBlockEntity;
+import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.block.entity.SkullBlockEntity;
+import net.minecraft.text.Text;
+
+public class BlockEntityRenderers {
+
+	public static boolean needsRenderer(BlockEntity blockEntity) {
+		if (blockEntity instanceof ChestBlockEntity || blockEntity instanceof EnderChestBlockEntity) {
+			return ChestAnimation.isOpen(blockEntity);
+		}
+
+		if (blockEntity instanceof SkullBlockEntity) {
+			return ((SkullBlockEntity) blockEntity).getType() == SkullType.PLAYER_ID;
+		}
+
+		if (blockEntity instanceof SignBlockEntity) {
+			return hasText((SignBlockEntity) blockEntity);
+		}
+
+		return true;
+	}
+
+	private static boolean hasText(SignBlockEntity sign) {
+		for (Text line : sign.lines) {
+			if (line != null && !line.getString().isEmpty()) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
--- src/main/java/org/codeberg/awruff/nsbe/mixin/compat/argentum/ChunkBuilderMeshingTaskMixin.java
+++ src/main/java/org/codeberg/awruff/nsbe/mixin/compat/argentum/ChunkBuilderMeshingTaskMixin.java
@@ -1,23 +1,32 @@
-package org.codeberg.awruff.nsbe.mixin.render.world;
+package org.codeberg.awruff.nsbe.mixin.compat.argentum;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
-import net.minecraft.client.render.world.RenderChunk;
 import org.codeberg.awruff.nsbe.impl.BlockEntityRenderers;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(RenderChunk.class)
-abstract class RenderChunkMixin {
+@Pseudo
+@Mixin(
+	targets = {
+		"dev.rdh.argentum.impl.render.terrain.compile.task.ChunkBuilderMeshingTask",
+		"org.taumc.celeritas.impl.render.terrain.compile.task.ChunkBuilderMeshingTask" // for older argentum builds
+	}
+)
+abstract class ChunkBuilderMeshingTaskMixin {
 
+	@Dynamic("Argentum")
 	@ModifyExpressionValue(
-		method = "compile",
+		method = "executeTimed",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;getRenderer(Lnet/minecraft/block/entity/BlockEntity;)Lnet/minecraft/client/render/block/entity/BlockEntityRenderer;"
-		)
+		),
+		require = 0
 	)
 	private BlockEntityRenderer<BlockEntity> nsbe$skipBakedRenderer(BlockEntityRenderer<BlockEntity> renderer, @Local BlockEntity blockEntity) {
 		if (blockEntity != null && !BlockEntityRenderers.needsRenderer(blockEntity)) {
